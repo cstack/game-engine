f15393ee7a056c3611bbe03e4e9a6cd41719aa74
Connor Stack <sirconnorstack@gmail.com>
2015-11-30 16:55:38 -0500

    Render a blank white canvas.

--- game.cc
+++ game.cc
@@ -1,16 +1,43 @@
 #include <ppapi_simple/ps_main.h>
+#include <ppapi_simple/ps_context_2d.h>
 #include <iostream>
 
+void update(double dt, PSContext2D_t* ctx) {
+  // Update and render
+  PSContext2DGetBuffer(ctx);
+
+  if (NULL == ctx->data) return;
+
+  // Clear to WHITE
+  memset(ctx->data, 0xFF, ctx->stride * ctx->height);
+
+  PSContext2DSwapBuffer(ctx);
+}
+
 int game_main(int argc, char* argv[]) {
-  for (int i=0; i<argc; ++i) {
-    std::cout << "Argument " << i << ": " << argv[i] << std::endl;
+
+  PSEventSetFilter(PSE_ALL);
+
+  PSContext2D_t* ctx = PSContext2DAllocate(PP_IMAGEDATAFORMAT_BGRA_PREMUL);
+  while (1) {
+    PSEvent* event;
+
+    // Consume all available events
+    while ((event = PSEventTryAcquire()) != NULL) {
+      PSContext2DHandleEvent(ctx, event);
+      PSEventRelease(event);
+    }
+
+    if (ctx->bound) {
+      update(1.0/30, ctx);
+    } else {
+      // If not bound, wait for an event which may signal a context becoming
+      // available, instead of spinning.
+      event = PSEventWaitAcquire();
+      PSContext2DHandleEvent(ctx, event);
+      PSEventRelease(event);
+    }
   }
-
-  std::cerr << "Standard error output appears in the debug console\n";
-
-  // Since PNaCl modules aren't supposed to terminate, we need to give
-  // Pepper some breathing room to convey all the messages:
-  sleep(3);
 
   return 0;
 }
