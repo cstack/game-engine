c4989a1ddc38f2fd5cffa2ed1e50937a13736622
Connor Stack <sirconnorstack@gmail.com>
2015-11-30 21:16:45 -0500

    Limit updates to a target framerate.

--- game.cc
+++ game.cc
@@ -1,6 +1,9 @@
 #include <ppapi_simple/ps_main.h>
 #include <ppapi_simple/ps_context_2d.h>
 #include <iostream>
+#include <sys/time.h>
+
+double TARGET_FRAME_RATE = 60;
 
 void update(double dt, PSContext2D_t* ctx) {
   // Update and render
@@ -14,11 +17,20 @@ void update(double dt, PSContext2D_t* ctx) {
   PSContext2DSwapBuffer(ctx);
 }
 
+double seconds() {
+  struct timeval tp;
+  gettimeofday(&tp, NULL);
+  return tp.tv_sec + tp.tv_usec / 1000000.0;
+}
+
 int game_main(int argc, char* argv[]) {
 
   PSEventSetFilter(PSE_ALL);
 
   PSContext2D_t* ctx = PSContext2DAllocate(PP_IMAGEDATAFORMAT_BGRA_PREMUL);
+  double timeLastStep;
+  double timeThisStep;
+  double dt;
   while (1) {
     PSEvent* event;
 
@@ -29,7 +41,13 @@ int game_main(int argc, char* argv[]) {
     }
 
     if (ctx->bound) {
-      update(1.0/30, ctx);
+      if(timeLastStep == 0)
+        timeLastStep = seconds();
+      timeThisStep = seconds();
+      dt = timeThisStep - timeLastStep;
+      update(dt, ctx);
+      timeLastStep = timeThisStep;
+      usleep(1000000/TARGET_FRAME_RATE);
     } else {
       // If not bound, wait for an event which may signal a context becoming
       // available, instead of spinning.
