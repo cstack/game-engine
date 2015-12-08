c2ab03f9db5ef623ea13adf907964ef2addbf498
Connor Stack <sirconnorstack@gmail.com>
2015-12-07 21:48:33 -0800

    Define colors in platform header file.

--- game.cc
+++ game.cc
@@ -3,28 +3,6 @@
 #include <string.h>
 #include <iostream>
 
-uint WHITE = 0xFFFFFFFF;
-uint BLUE = 0x000000FF;
-uint GREEN = 0x0000FF00;
-uint colors[] = {
-  0x00000000,
-  0x000000FF,
-  0x0000FF00,
-  0x0000FFFF,
-  0x00FF0000,
-  0x00FF00FF,
-  0x00FFFF00,
-  0x00FFFFFF,
-  0xFF000000,
-  0xFF0000FF,
-  0xFF00FF00,
-  0xFF00FFFF,
-  0xFFFF0000,
-  0xFFFF00FF,
-  0xFFFFFF00,
-  0xFFFFFFFF,
-};
-
 double clip(double value, double min, double max) {
   if (value < min)
     value = min;
@@ -33,12 +11,12 @@ double clip(double value, double min, double max) {
   return value;
 }
 
-void put_pixel(pixel_buffer_t* pixel_buffer, uint x, uint y, uint color) {
+void put_pixel(pixel_buffer_t* pixel_buffer, uint x, uint y, color_t color) {
   uint offset = pixel_buffer->width * y + x;
   pixel_buffer->data[offset] = color;
 }
 
-void draw_box(pixel_buffer_t* pixel_buffer, double x, double y, double width, double height, uint color) {
+void draw_box(pixel_buffer_t* pixel_buffer, double x, double y, double width, double height, color_t color) {
   int x1 = clip(x, 0, pixel_buffer->width);
   int x2 = clip(x+width, 0, pixel_buffer->width);
   int y1 = clip(y, 0, pixel_buffer->height);
@@ -50,21 +28,6 @@ void draw_box(pixel_buffer_t* pixel_buffer, double x, double y, double width, do
     }
   }
 }
-
-void draw_bars(pixel_buffer_t* pixel_buffer) {
-  uint num_sections = 16;
-  uint section_height = pixel_buffer->height / num_sections;
-  uint section_size = section_height * pixel_buffer->width;
-  for (int i=0; i<num_sections; i++) {
-    uint color = colors[i];
-    for (int j=0; j< section_size; j++) {
-      uint offset = section_size*i + j;
-      pixel_buffer->data[offset] = color;
-    }
-  }
-}
-
-
 
 void update(double dt, pixel_buffer_t* pixel_buffer) {
   // Update and render
--- platform/platform.h
+++ platform/platform.h
@@ -1,10 +1,22 @@
 #include <stdint.h>
 
+typedef uint32_t pixel_t;
+typedef pixel_t color_t;
+
 struct pixel_buffer_t {
   int32_t height;
   uint32_t width;
   uint32_t stride;
-  uint32_t* data;
+  pixel_t* data;
 };
+
+color_t BLACK = 0x00000000;
+color_t BLUE = 0x000000FF;
+color_t GREEN = 0x0000FF00;
+color_t CYAN = 0x0000FFFF;
+color_t RED = 0x00FF0000;
+color_t MAGENTA = 0x00FF00FF;
+color_t YELLOW = 0x00FFFF00;
+color_t WHITE = 0x00FFFFFF;
 
 void update(double dt, pixel_buffer_t* pixel_buffer);
