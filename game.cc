52e7e936aae517f4fc1d2064a728f668ec21e48a
Connor Stack <sirconnorstack@gmail.com>
2015-12-12 22:10:26 -0800

    Support double rgb values.

--- game.cc
+++ game.cc
@@ -3,6 +3,17 @@
 #include <string.h>
 #include <iostream>
 
+int round(double value) {
+  return (int) (value + 0.5);
+}
+
+color_t rgb(double red, double green, double blue) {
+  uint red_component = round(red) << 16;
+  uint green_component = round(green) << 8;
+  uint blue_component = round(blue);
+  return red_component | green_component | blue_component;
+}
+
 double clip(double value, double min, double max) {
   if (value < min)
     value = min;
@@ -31,7 +42,8 @@ void draw_box(pixel_buffer_t* pixel_buffer, double x, double y, double width, do
 
 void update(double dt, pixel_buffer_t* pixel_buffer) {
   // Update and render
-  draw_box(pixel_buffer, -10, 90, 40, 10, WHITE);
+  color_t color = rgb(150.3, 250.5, 20.6);
+  draw_box(pixel_buffer, -10, 90, 40, 10, color);
   draw_box(pixel_buffer, 30, 20, 100, 50, BLUE);
   draw_box(pixel_buffer, 40, 30, 20, 140, GREEN);
 }
