13fc999de4175cdbb776c247f713091d91e9b117
Connor Stack <sirconnorstack@gmail.com>
2015-12-20 10:52:38 -0500

    Make colors constant.

--- platform/platform.h
+++ platform/platform.h
@@ -10,13 +10,13 @@ struct pixel_buffer_t {
   pixel_t* data;
 };
 
-color_t BLACK = 0x00000000;
-color_t BLUE = 0x000000FF;
-color_t GREEN = 0x0000FF00;
-color_t CYAN = 0x0000FFFF;
-color_t RED = 0x00FF0000;
-color_t MAGENTA = 0x00FF00FF;
-color_t YELLOW = 0x00FFFF00;
-color_t WHITE = 0x00FFFFFF;
+const color_t BLACK = 0x00000000;
+const color_t BLUE = 0x000000FF;
+const color_t GREEN = 0x0000FF00;
+const color_t CYAN = 0x0000FFFF;
+const color_t RED = 0x00FF0000;
+const color_t MAGENTA = 0x00FF00FF;
+const color_t YELLOW = 0x00FFFF00;
+const color_t WHITE = 0x00FFFFFF;
 
 void update(double dt, pixel_buffer_t* pixel_buffer);
