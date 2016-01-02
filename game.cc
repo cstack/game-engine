d5418b1caf0f96bff42b4dbed8daf2fe2b696ab0
Connor Stack <sirconnorstack@gmail.com>
2016-01-02 13:22:22 -0800

    Collide player position with tile map.

--- game.cc
+++ game.cc
@@ -5,10 +5,13 @@
 #include <string.h>
 #include <iostream>
 
+const uint SCREEN_WIDTH = 1024;
+const uint SCREEN_HEIGHT = 576;
+
 const uint TILE_MAP_WIDTH = 16;
 const uint TILE_MAP_HEIGHT = 9;
-const uint TILE_WIDTH = 1024 / TILE_MAP_WIDTH;
-const uint TILE_HEIGHT = 576 / TILE_MAP_HEIGHT;
+const uint TILE_WIDTH = SCREEN_WIDTH / TILE_MAP_WIDTH;
+const uint TILE_HEIGHT = SCREEN_HEIGHT / TILE_MAP_HEIGHT;
 const color_t TILE_COLOR = rgb(100, 100, 100);
 const bool TILE_MAP[TILE_MAP_HEIGHT][TILE_MAP_WIDTH] = {
   {1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1},
@@ -63,6 +66,16 @@ void clear_screen(pixel_buffer_t* pixel_buffer) {
   draw_box(pixel_buffer, 0, 0, pixel_buffer->width, pixel_buffer->height, BLACK);
 }
 
+bool valid_player_location(double x, double y) {
+  if (x < 0 || y < 0 || x > SCREEN_WIDTH || y > SCREEN_HEIGHT) {
+    return false;
+  }
+  uint row = y / TILE_HEIGHT;
+  uint col = x / TILE_WIDTH;
+  bool occupied = TILE_MAP[row][col];
+  return !occupied;
+}
+
 void update(double dt, pixel_buffer_t* pixel_buffer, controller_t &controller) {
   if (!game_state.initialized) {
     initialize_game_state(game_state);
@@ -70,19 +83,29 @@ void update(double dt, pixel_buffer_t* pixel_buffer, controller_t &controller) {
 
   clear_screen(pixel_buffer);
 
+  // Move player
+  double new_player_x = game_state.player_x;
+  double new_player_y = game_state.player_y;
   if (controller.right_pressed) {
-    game_state.player_x += PLAYER_SPEED * dt;
+    new_player_x += PLAYER_SPEED * dt;
   }
   if (controller.left_pressed) {
-    game_state.player_x -= PLAYER_SPEED * dt;
+    new_player_x -= PLAYER_SPEED * dt;
   }
   if (controller.up_pressed) {
-    game_state.player_y -= PLAYER_SPEED * dt;
+    new_player_y -= PLAYER_SPEED * dt;
   }
   if (controller.down_pressed) {
-    game_state.player_y += PLAYER_SPEED * dt;
+    new_player_y += PLAYER_SPEED * dt;
+  }
+  if (valid_player_location(new_player_x, game_state.player_y)) {
+    game_state.player_x = new_player_x;
+  }
+  if (valid_player_location(game_state.player_x, new_player_y)) {
+    game_state.player_y = new_player_y;
   }
 
+  // Render tile map
   for (int row=0; row < TILE_MAP_HEIGHT; row++) {
     for (int col=0; col < TILE_MAP_WIDTH; col++) {
       if (TILE_MAP[row][col]) {
@@ -98,6 +121,7 @@ void update(double dt, pixel_buffer_t* pixel_buffer, controller_t &controller) {
     }
   }
 
+  // Render Player
   draw_box(
     pixel_buffer,
     game_state.player_x - PLAYER_WIDTH/2,
